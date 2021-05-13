e07fafd1d076481c6fe3c0f85695c98e2dc976d6
Pharap <2933055+Pharap@users.noreply.github.com>
2021-05-13 16:35:17 +0100

    Delete src directory

--- src/emptyRoom.h
+++
@@ -1,6 +0,0 @@
-#ifndef emptyRoom_h
-#define emptyRoom_h
-
-const uint8_t PROGMEM empty_room[] = { 0 };
-
-#endif
--- src/loadEntity.h
+++
@@ -1,9 +0,0 @@
-#ifndef loadEntity_h
-#define loadEntity_h
-
-#include "entity.h"
-
-void loadEntity(Entity& entity, EntityType entityType);
-
-#endif
-
